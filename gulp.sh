5333c632853e7da2f5100c4727af0a257eae3ed0
Jose Maestre <jose.maestre@linio.com>
2015-12-09 17:00:17 -0430

    primer commit

---
+++ gulp.sh
@@ -0,0 +1,6 @@
+sudo npm install gulp-if
+sudo npm install gulp-uglify
+sudo npm install gulp-uglifycss
+sudo npm install gulp-less
+sudo npm install gulp-concat
+sudo npm install gulp-sourcemaps
---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+npm install gulp-if
+npm install gulp-uglify
+npm install gulp-uglifycss
+npm install gulp-less
+npm install gulp-concat
+npm install gulp-sourcemaps
